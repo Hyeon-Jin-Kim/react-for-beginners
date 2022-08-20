efe41c52e4bf86567f1aaa71c64ee840fedc1cf3
Hyeon-Jin-Kim <68842782+Hyeon-Jin-Kim@users.noreply.github.com>
2022-08-20 15:39:21 +0900

    create react app - effect

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,10 +1,47 @@
-import Button from "./Button";
-import styled from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("Hi :)");
+    return () => console.log("bye :(");
+  }, []);
+
+  return <h1>Hello</h1>;
+}
+
 function App() {
+  /* const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("I run only once...");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("I run when 'keyword' changes.");
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+ */
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+
   return (
     <div>
-      <h1 className={styled.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      {/*       <input
+        value={keyword}
+        onChange={onChange}
+        type='text'
+        placeholder='Search here..'
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me </button> */}
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
