1afb36831d47baeddc608527deea3406ccbc172d
Hyeon-Jin-Kim <68842782+Hyeon-Jin-Kim@users.noreply.github.com>
2022-08-25 05:17:49 +0900

    current

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -3,14 +3,12 @@ import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/movie' element={<Detail />} />
-      </Routes>
-    </Router>
-  );
+  <Router>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/movie' element={<Detail />} />
+    </Routes>
+  </Router>;
 }
 
 export default App;
